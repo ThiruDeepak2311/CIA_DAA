d888c8cdbdb853969eb48dc5f4b8cf35e2d9d108
ThiruDeepak2311 <116452492+ThiruDeepak2311@users.noreply.github.com>
2023-01-11 10:10:00 +0530

    Update cia_1.py

--- cia_1.py
+++ cia_1.py
@@ -1,57 +1,68 @@
 import numpy as np
 
-def NeedlemanWunsch(a, b, i, j, score, penalty, arr, Parent):
-    if i == 0 and j == 0:
-        return 0
-    elif arr[i][j] != -1:
-        return arr[i][j]
+import random as rd 
+chars = ['a','c','g','t']
+a  = ""
+b = ""
+l = 16
+a_ct = c_ct = g_ct = t_ct = 0
+
+while len(a) < l:
+    ch = rd.choice(chars)
+    if ch=='a' and a_ct<4:
+        a+=ch
+        a_ct+=1
+    elif ch=='c' and c_ct<4:
+        a +=ch
+        c_ct+=1
+    elif ch=='g' and g_ct < 4:
+        a +=ch 
+        g_ct+=1
+    elif ch == 't' and t_ct < 4:
+        a +=ch
+        t_ct+=1
+
+a_ct = c_ct = g_ct = t_ct = 0
+
+while len(b) < l:
+    ch = rd.choice(chars)
+    if ch=='a' and a_ct<4:
+        b+=ch
+        a_ct+=1
+    elif ch=='c' and c_ct<4:
+        b+=ch
+        c_ct+=1
+    elif ch=='g' and g_ct < 4:
+        b+=ch 
+        g_ct+=1
+    elif ch == 't' and t_ct < 4:
+        b+=ch
+        t_ct+=1
+print(a)
+print(b)
+cols = len(b)
+rows = len(a)
+score = 5
+penalty = -4
+cols = len(b)
+rows = len(a)
+arr = np.zeros((rows+1, cols+1), dtype=int)
+
+i=1 #row
+j=1 #col 
+
+def align(arr,i,j):
+    if a[i-1] == b[j-1]:
+            arr[i][j] = arr[i-1][j-1]+score
     else:
-        if i > 0 and j > 0:
-            if a[i-1] == b[j-1]:
-                s = score
-                arr[i][j] = NeedlemanWunsch(a, b, i-1, j-1, score, penalty, arr, Parent) + s
-                Parent[i][j] = (i-1, j-1)
-            else:
-                s = penalty
-                diag = NeedlemanWunsch(a, b, i-1, j-1, score, penalty, arr, Parent)
-                up = NeedlemanWunsch(a, b, i-1, j, score, penalty, arr, Parent)
-                left = NeedlemanWunsch(a, b, i, j-1, score, penalty, arr, Parent)
-                arr[i][j] = max(diag, up, left) + s
-                if diag == arr[i][j]-s:
-                    Parent[i][j] = (i-1, j-1)
-                elif up == arr[i][j]-s:
-                    Parent[i][j] = (i-1, j)
-                elif left == arr[i][j]-s:
-                    Parent[i][j] = (i, j-1)
-        elif i > 0:
-            s = penalty
-            arr[i][j] = NeedlemanWunsch(a, b, i-1, j, score, penalty, arr, Parent) + s
-            Parent[i][j] = (i-1, j)
-        else:
-            s = penalty
-            arr[i][j] = NeedlemanWunsch(a, b, i, j-1, score, penalty, arr, Parent) + s
-            Parent[i][j] = (i, j-1)
-        return arr[i][j]
-
-def AlignSequences(a, b, score, penalty):
-    rows = len(a)
-    cols = len(b)
-    arr = np.zeros((rows+1, cols+1), dtype=int)
-    arr.fill(-1)
-    Parent = [[0]*(cols+1) for i in range(rows+1)]
-    max_score = NeedlemanWunsch(a, b, rows, cols, score, penalty, arr, Parent)
-
-    ans = []
-    p = []
-    i, j = rows, cols
-    while(i > 0 or j > 0):
-        ans.append(arr[i][j])
-        p.append((i,j))
-        idx = Parent[i][j]
-        i, j = idx[0], idx[1]
-
-    s1 = ''
-    s2 = ''
-    for i in range(len(p)-1):
-        t = p[i]
-       
+            m = max(arr[i-1][j-1], arr[i-1][j], arr[i][j-1])
+            arr[i][j] = m + penalty  
+    if j!=len(b):
+        return align(arr,i,j+1)
+    elif i==len(a):
+        print(arr)
+    else:
+        j=1
+        return align(arr,i+1,j)
+        
+align(arr,i,j)
